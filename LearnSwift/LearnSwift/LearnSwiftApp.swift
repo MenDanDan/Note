c39069d6ddc510e0fe96afa4583167e5359128c6
MenDanDan <528367007@qq.com>
2023-10-07 14:48:50 +0800

    创建项目

---
+++ LearnSwift/LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by dandanmen on 2023/10/7.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
